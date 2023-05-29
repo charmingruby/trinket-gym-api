0397672c7735775b4751514d153b36c6b29da61f
charmingruby <mrrubycanal3105@gmail.com>
2023-05-28 23:32:54 -0300

    create user use case

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,25 @@
 import fastify from 'fastify'
+import { ZodError } from 'zod'
+import { appRoutes } from './http/routes'
+import { env } from './env'
 
 export const app = fastify()
+
+app.register(appRoutes)
+
+app.setErrorHandler((error, _req, rep) => {
+  if (error instanceof ZodError) {
+    return rep.status(400).send({
+      message: 'Validation error.',
+      issues: error.format(),
+    })
+  }
+
+  if (env.NODE_ENV !== 'production') {
+    console.error(error)
+  }
+
+  return rep.status(500).send({
+    message: 'Internal server error.',
+  })
+})
---
+++ src/http/controllers/register.ts
@@ -0,0 +1,36 @@
+import { z } from 'zod'
+import { FastifyReply, FastifyRequest } from 'fastify'
+import { RegisterUseCase } from '@/use-cases/register'
+import { PrismaUsersRepository } from '@/repositories/prisma/prisma-users-repository'
+import { UserAlreadyExistsError } from '@/use-cases/errors/user-already-exists-error'
+
+export async function register(req: FastifyRequest, rep: FastifyReply) {
+  const registerBodySchema = z.object({
+    name: z.string(),
+    email: z.string().email(),
+    password: z.string().min(6),
+  })
+
+  const { name, email, password } = registerBodySchema.parse(req.body)
+
+  try {
+    const usersRepository = new PrismaUsersRepository()
+    const registerUseCase = new RegisterUseCase(usersRepository)
+
+    await registerUseCase.execute({
+      name,
+      email,
+      password,
+    })
+  } catch (err) {
+    if (err instanceof UserAlreadyExistsError) {
+      return rep.status(409).send({
+        message: err.message,
+      })
+    }
+
+    throw err
+  }
+
+  return rep.status(201).send()
+}
